74ebc20b56e6c030174916d266f0d950c7625608
Michal Dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-07-01 00:33:05 +0200

    test

--- server.js
+++ server.js
@@ -46,4 +46,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`)); 
+app.listen(port, () => console.log(`Server started on port ${port}`));  
