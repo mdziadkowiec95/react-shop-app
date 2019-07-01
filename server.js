a0b2631480f6f11d45f277fda42c1c2d4fcec7fb
Michal Dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-07-01 21:35:41 +0200

    added static upload dir

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-/* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const products = require('./routes/api/products');
 const path = require('path');
 
 const app = express();
 
+// Body parser Middleware
+app.use('/uploads', express.static('uploads'));
+
+
+
 app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
 
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, 'uploads/')
+  }
+});
+
+const upload = multer({ storage: storage });
+
 /** Connect to Mongo DB */
-mongoose
-  .connect(db)
+mongoose.connect(db)
   .then(() => console.log('Mongo DB connected!'))
   .catch(err => console.log(err));
 
-app.use('/uploads', express.static('uploads'));
 app.use('/api/products', products);
 
 /** Serve static assets if in production */
@@ -29,10 +40,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));  
